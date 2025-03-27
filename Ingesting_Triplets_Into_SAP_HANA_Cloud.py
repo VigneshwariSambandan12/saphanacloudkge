2b006111972af3e47feab1e12d5ab353cbfa2e32
DCosta <priya.ronald.dcosta@sap.com>
2025-03-27 09:35:04 -0400

    iupdated as per Markus' suggestions

--- Ingesting_Triplets_Into_SAP_HANA_Cloud.py
+++ Ingesting_Triplets_Into_SAP_HANA_Cloud.py
@@ -10,28 +10,32 @@ conn = dbapi.connect(
 )
 
 
+triples = []
+
 # Call stored procedure to execute SPARQL query or insert RDF triples
 cursor = conn.cursor()
 try:
     # Iterate through the triples and insert them into the database
-    for s, p, o in rdf_graph:
-        # Construct a SPARQL INSERT query for each triple
-        sparql_insert_query = f"""
-            INSERT DATA {{
-                <{s}> <{p}> <{o}> .
-            }}
-        """
+    for s, p, o in rdf_triples: #NOTE: This is the RDF triples object from the "Generating Triples file"
+        
+        # Append the formatted triple to the list
+        triples.append(f"<{str(s)}> <{str(p)}> <{str(o)}>")
 
-        # Execute the SPARQL query using the stored procedure
-        resp = cursor.callproc('SPARQL_EXECUTE', (sparql_insert_query, 'Metadata headers describing Input and/or Output', '?', None))
+    # Join triples into SPARQL statement
+    triples_str = " .\n    ".join(triples) + " ."
+    
+    sparql_insert_query = f"INSERT DATA {{ \n    {triples_str} \n}}"
 
-        # Handle response if necessary
-        metadata_headers = resp[3]  # OUT: RQX Response Metadata/Headers
-        query_response = resp[2]     # OUT: RQX Response
+    # Execute the SPARQL query using the stored procedure
+    resp = cursor.callproc('SPARQL_EXECUTE', (sparql_insert_query,"Accept: application/sparql-results+xml Content-Type: application/sparql-query", '?', None))
 
-        print("Inserted Triple:", (s, p, o))
-        print("Response Metadata:", metadata_headers)
-        print("Query Response:", query_response)
+    # Handle response if necessary
+    metadata_headers = resp[3]  # OUT: RQX Response Metadata/Headers
+    query_response = resp[2]     # OUT: RQX Response
+
+    print("Inserted Triple:", (s, p, o))
+    print("Response Metadata:", metadata_headers)
+    print("Query Response:", query_response)
 
 finally:
     cursor.close()
